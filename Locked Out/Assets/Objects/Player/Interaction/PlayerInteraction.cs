a122ab0455f3876c715fba134c36eb406ddbf9e4
Joeri van Alphen <jralphen@kpnmail.nl>
2024-01-06 18:42:26 +0100

    Updated interaction component

--- Locked Out/Assets/Objects/Player/Interaction/PlayerInteraction.cs
+++ Locked Out/Assets/Objects/Player/Interaction/PlayerInteraction.cs
@@ -8,12 +8,36 @@ public class PlayerInteraction : MonoBehaviour
     [Space]
     [SerializeField] private Transform m_raycastOrigin;
 
+    private IInteractable m_cachedInteractable = null;
+
     private void Update()
     {
-        if (!Input.GetKeyDown(KeyCode.Space)) return;
-        if (!Physics.Raycast(m_raycastOrigin.position, m_raycastOrigin.forward, out RaycastHit _hit, m_interactionDistance)) return;
-        if (!_hit.transform.TryGetComponent(out IInteractable _interactable)) return;
+        //  Check if our interaction raycast hits an interactable object. If it didn't clear the cached interactable.
+        if (!FoundInteractable(out IInteractable _interactable))
+        {
+            if (m_cachedInteractable != null)
+            {
+                m_cachedInteractable.OnExit();
+                m_cachedInteractable = null;
+            }
+            return;
+        }
 
-        _interactable.Interact();
+        //  If a valid interactable is found, cache it, and call OnEnter function. 
+        m_cachedInteractable?.OnExit();
+        m_cachedInteractable = _interactable;
+        m_cachedInteractable.OnEnter();
+
+        //  If the interaction button has been pressed
+        if (Input.GetKeyDown(KeyCode.Space)) _interactable.OnInteract();
+    }
+
+    private bool FoundInteractable(out IInteractable _interactable)
+    {
+        _interactable = null;
+
+        if (!Physics.Raycast(m_raycastOrigin.position, m_raycastOrigin.forward, out RaycastHit _hit, m_interactionDistance)) return false;
+        if (!_hit.transform.TryGetComponent(out _interactable)) return false;
+        return true;
     }
 }
