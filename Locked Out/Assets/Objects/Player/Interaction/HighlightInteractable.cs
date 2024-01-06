3b65c8fcfe272cacaa23f2a99c14aa81ef3746a1
Joeri van Alphen <jralphen@kpnmail.nl>
2024-01-06 18:42:45 +0100

    Added abstract 'HighlightInteractable' class

---
+++ Locked Out/Assets/Objects/Player/Interaction/HighlightInteractable.cs
@@ -0,0 +1,25 @@
+﻿using Highlighters;
+using UnityEngine;
+
+[RequireComponent(typeof(Highlighter))]
+public abstract class HighlightInteractable : MonoBehaviour, IInteractable
+{
+    private Highlighter m_hightlighter = null;
+
+    private void Awake()
+    {
+        m_hightlighter = GetComponent<Highlighter>();
+    }
+
+    public virtual void OnEnter()
+    {
+       if (m_hightlighter != null) m_hightlighter.enabled = true;
+    }
+
+    public virtual void OnExit()
+    {
+        if (m_hightlighter != null) m_hightlighter.enabled = false;
+    }
+
+    public abstract void OnInteract();
+}
